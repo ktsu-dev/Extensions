84b92fb472a4d4b089b71dcf23017160266826b6
matt-edmondson <matthew.edmondson@gmail.com>
2024-02-18 14:12:04 +1100

    Add String extensions for StrongStrings

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -1,5 +1,7 @@
 namespace ktsu.io.Extensions;
 
+using ktsu.io.StrongStrings;
+
 /// <summary>
 /// Extension methods for strings.
 /// </summary>
@@ -12,6 +14,19 @@ public static class StringExtensions
 	/// <param name="value">The <paramref name="value"/> to compare to.</param>
 	/// <returns>true if str starts with value; otherwise, false.</returns>
 	public static bool StartsWithOrdinal(this string str, string value)
+	{
+		ArgumentNullException.ThrowIfNull(str);
+		ArgumentNullException.ThrowIfNull(value);
+		return str.StartsWith(value, StringComparison.Ordinal);
+	}
+
+	/// <summary>
+	/// Method that compares two strings using ordinal comparison.
+	/// </summary>
+	/// <param name="str">The string to compare.</param>
+	/// <param name="value">The <paramref name="value"/> to compare to.</param>
+	/// <returns>true if str starts with value; otherwise, false.</returns>
+	public static bool StartsWithOrdinal<TDerived>(this TDerived str, string value) where TDerived : AnyStrongString<TDerived>
 	{
 		ArgumentNullException.ThrowIfNull(str);
 		ArgumentNullException.ThrowIfNull(value);
@@ -36,8 +51,34 @@ public static class StringExtensions
 	/// </summary>
 	/// <param name="str">The string to compare.</param>
 	/// <param name="value">The <paramref name="value"/> to compare to.</param>
+	/// <returns>true if str ends with value; otherwise, false.</returns>
+	public static bool EndsWithOrdinal<TDerived>(this TDerived str, string value) where TDerived : AnyStrongString<TDerived>
+	{
+		ArgumentNullException.ThrowIfNull(str);
+		ArgumentNullException.ThrowIfNull(value);
+		return str.EndsWith(value, StringComparison.Ordinal);
+	}
+
+	/// <summary>
+	/// Method that compares two strings using ordinal comparison.
+	/// </summary>
+	/// <param name="str">The string to compare.</param>
+	/// <param name="value">The <paramref name="value"/> to compare to.</param>
 	/// <returns>true if str contains value; otherwise, false.</returns>
 	public static bool ContainsOrdinal(this string str, string value)
+	{
+		ArgumentNullException.ThrowIfNull(str);
+		ArgumentNullException.ThrowIfNull(value);
+		return str.Contains(value, StringComparison.Ordinal);
+	}
+
+	/// <summary>
+	/// Method that compares two strings using ordinal comparison.
+	/// </summary>
+	/// <param name="str">The string to compare.</param>
+	/// <param name="value">The <paramref name="value"/> to compare to.</param>
+	/// <returns>true if str contains value; otherwise, false.</returns>
+	public static bool ContainsOrdinal<TDerived>(this TDerived str, string value) where TDerived : AnyStrongString<TDerived>
 	{
 		ArgumentNullException.ThrowIfNull(str);
 		ArgumentNullException.ThrowIfNull(value);
@@ -59,6 +100,20 @@ public static class StringExtensions
 	}
 
 	/// <summary>
+	/// Removes the specified <paramref name="suffix"/> from the current string.
+	/// </summary>
+	/// <param name="s">The string to remove the suffix from.</param>
+	/// <param name="suffix">The suffix to remove.</param>
+	/// <returns>The string with the suffix removed.</returns>
+	public static TDerived RemoveSuffix<TDerived>(this TDerived s, string suffix) where TDerived : AnyStrongString<TDerived>
+	{
+		ArgumentNullException.ThrowIfNull(s);
+		ArgumentNullException.ThrowIfNull(suffix);
+
+		return (TDerived)(s.EndsWithOrdinal(suffix) ? s.WeakString[..^suffix.Length] : s);
+	}
+
+	/// <summary>
 	/// Removes the specified <paramref name="prefix"/> from the current string.
 	/// </summary>
 	/// <param name="s">The string to remove the prefix from.</param>
@@ -70,5 +125,19 @@ public static class StringExtensions
 		ArgumentNullException.ThrowIfNull(prefix);
 
 		return s.StartsWithOrdinal(prefix) ? s[prefix.Length..] : s;
+	}
+
+	/// <summary>
+	/// Removes the specified <paramref name="prefix"/> from the current string.
+	/// </summary>
+	/// <param name="s">The string to remove the prefix from.</param>
+	/// <param name="prefix">The prefix to remove.</param>
+	/// <returns>The string with the prefix removed.</returns>
+	public static TDerived RemovePrefix<TDerived>(this TDerived s, string prefix) where TDerived : AnyStrongString<TDerived>
+	{
+		ArgumentNullException.ThrowIfNull(s);
+		ArgumentNullException.ThrowIfNull(prefix);
+
+		return (TDerived)(s.StartsWithOrdinal(prefix) ? s.WeakString[prefix.Length..] : s);
 	}
 }
