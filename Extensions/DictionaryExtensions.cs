6a6d6e12c57c87f978e4f03b67916dd9479e78a7
matt-edmondson <matthew.edmondson@gmail.com>
2024-06-20 17:24:25 +1000

    Add support for GetOrCreate on concurrent dictionaries

--- Extensions/DictionaryExtensions.cs
+++ Extensions/DictionaryExtensions.cs
@@ -1,6 +1,8 @@
 namespace ktsu.io.Extensions;
 
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 /// <summary>
 /// Extension methods for dictionaries.
@@ -12,32 +14,57 @@ public static class DictionaryExtensions
 	/// </summary>
 	/// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
 	/// <typeparam name="TVal">The type of the values in the dictionary.</typeparam>
-	/// <param name="dict">The dictionary to get the value from.</param>
+	/// <param name="dictionary">The dictionary to get the value from.</param>
 	/// <param name="key">The key to get the value for.</param>
 	/// <returns>The value for the key if it exists, otherwise a new value.</returns>
-	public static TVal GetOrCreate<TKey, TVal>(this Dictionary<TKey, TVal> dict, TKey key) where TKey : notnull where TVal : new() => GetOrCreate(dict, key, new TVal());
+	public static TVal GetOrCreate<TKey, TVal>(this Dictionary<TKey, TVal> dictionary, TKey key) where TKey : notnull where TVal : new() => GetOrCreate(dictionary, key, new TVal());
 
 	/// <summary>
 	/// Method that gets a value from a dictionary if it exists, otherwise creates a new value and adds it to the dictionary.
 	/// </summary>
 	/// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
 	/// <typeparam name="TVal">The type of the values in the dictionary.</typeparam>
-	/// <param name="dict">The dictionary to get the value from.</param>
+	/// <param name="dictionary">The dictionary to get the value from.</param>
 	/// <param name="key">The key to get the value for.</param>
 	/// <param name="defaultValue">The default value to add when an existing value is not found.</param>
 	/// <returns>The value for the key if it exists, otherwise a new value.</returns>
-	public static TVal GetOrCreate<TKey, TVal>(this Dictionary<TKey, TVal> dict, TKey key, TVal defaultValue) where TKey : notnull where TVal : new()
+	public static TVal GetOrCreate<TKey, TVal>(this Dictionary<TKey, TVal> dictionary, TKey key, TVal defaultValue) where TKey : notnull where TVal : new()
 	{
-		ArgumentNullException.ThrowIfNull(dict, nameof(dict));
-		ArgumentNullException.ThrowIfNull(key, nameof(key));
-		ArgumentNullException.ThrowIfNull(defaultValue, nameof(defaultValue));
+		ArgumentNullException.ThrowIfNull(dictionary);
+		ArgumentNullException.ThrowIfNull(key);
+		ArgumentNullException.ThrowIfNull(defaultValue);
 
-		if (dict.TryGetValue(key, out var val))
+		if (dictionary.TryGetValue(key, out var val))
 		{
 			return val;
 		}
 
-		dict.Add(key, defaultValue);
+		dictionary.Add(key, defaultValue);
+		return defaultValue;
+	}
+
+	/// <summary>
+	/// Method that gets a value from a dictionary if it exists, otherwise creates a new value and adds it to the dictionary.
+	/// </summary>
+	/// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+	/// <typeparam name="TVal">The type of the values in the dictionary.</typeparam>
+	/// <param name="dictionary">The dictionary to get the value from.</param>
+	/// <param name="key">The key to get the value for.</param>
+	/// <param name="defaultValue">The default value to add when an existing value is not found.</param>
+	/// <returns>The value for the key if it exists, otherwise a new value.</returns>
+	public static TVal GetOrCreate<TKey, TVal>(this ConcurrentDictionary<TKey, TVal> dictionary, TKey key, TVal defaultValue) where TKey : notnull where TVal : new()
+	{
+		ArgumentNullException.ThrowIfNull(dictionary);
+		ArgumentNullException.ThrowIfNull(key);
+		ArgumentNullException.ThrowIfNull(defaultValue);
+
+		if (dictionary.TryGetValue(key, out var val))
+		{
+			return val;
+		}
+
+		bool result = dictionary.TryAdd(key, defaultValue);
+		Debug.Assert(result);
 		return defaultValue;
 	}
 }
