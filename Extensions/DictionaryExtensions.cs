78e857874bbeb61944f7c4f43ff98f65e9f62f97
Matt Edmondson <matthew.edmondson@gmail.com>
2025-05-22 13:00:36 +1000

    Remove deep cloning functionality and update SDK versions
    This commit removes the `ktsu.DeepClone` dependency from the test files and eliminates related test methods for deep cloning of dictionaries and collections. The `DeepClone` and `ShallowClone` methods have also been removed from `DictionaryExtensions.cs` and `EnumerableExtensions.cs`. Additionally, the project files have been updated to use the latest versions of the `ktsu.Sdk.Test` and `ktsu.Sdk.Lib` SDKs. The test structure has been simplified to focus on essential functionalities.

--- Extensions/DictionaryExtensions.cs
+++ Extensions/DictionaryExtensions.cs
@@ -7,8 +7,6 @@ namespace ktsu.Extensions;
 using System.Collections.Concurrent;
 using System.Diagnostics;
 
-using ktsu.DeepClone;
-
 /// <summary>
 /// Extension methods for dictionaries.
 /// </summary>
@@ -71,84 +69,6 @@ public static class DictionaryExtensions
 		var result = dictionary.TryAdd(key, defaultValue);
 		Debug.Assert(result);
 		return defaultValue;
-	}
-
-	/// <summary>
-	/// Creates a deep clone of a dictionary, returning a new dictionary with the same keys and deep-cloned values.
-	/// </summary>
-	/// <typeparam name="TKey">The type of the keys in the dictionary. Keys must be non-nullable.</typeparam>
-	/// <typeparam name="TValue">The type of the values in the dictionary, which must implement <see cref="IDeepCloneable{TValue}"/>.</typeparam>
-	/// <param name="items">The dictionary to clone. This dictionary cannot be null.</param>
-	/// <returns>A new dictionary with the same keys and deep-cloned values as the input dictionary.</returns>
-	/// <exception cref="ArgumentNullException">Thrown if the <paramref name="items"/> dictionary is null.</exception>
-	public static Dictionary<TKey, TValue> DeepClone<TKey, TValue>(this IDictionary<TKey, TValue> items)
-		where TKey : notnull
-		where TValue : class, IDeepCloneable<TValue>
-	{
-		ArgumentNullException.ThrowIfNull(items);
-		return items.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.DeepClone());
-	}
-
-	/// <summary>
-	/// Creates a deep clone of a dictionary, returning a new dictionary with the same keys and deep-cloned values, 
-	/// with thread-safety ensured by locking on the provided object.
-	/// </summary>
-	/// <typeparam name="TKey">The type of the keys in the dictionary. Keys must be non-nullable.</typeparam>
-	/// <typeparam name="TValue">The type of the values in the dictionary, which must implement <see cref="IDeepCloneable{TValue}"/>.</typeparam>
-	/// <param name="items">The dictionary to clone. This dictionary cannot be null.</param>
-	/// <param name="lockObj">The object to lock on to ensure thread-safety during the cloning operation. This cannot be null.</param>
-	/// <returns>A new dictionary with the same keys and deep-cloned values as the input dictionary.</returns>
-	/// <exception cref="ArgumentNullException">
-	/// Thrown if the <paramref name="items"/> dictionary or the <paramref name="lockObj"/> is null.
-	/// </exception>
-	public static Dictionary<TKey, TValue> DeepClone<TKey, TValue>(this IDictionary<TKey, TValue> items, object lockObj)
-		where TKey : notnull
-		where TValue : class, IDeepCloneable<TValue>
-	{
-		ArgumentNullException.ThrowIfNull(items);
-		ArgumentNullException.ThrowIfNull(lockObj);
-		lock (lockObj)
-		{
-			return DeepClone(items);
-		}
-	}
-
-	/// <summary>
-	/// Creates a shallow clone of a dictionary, returning a new dictionary with the same keys and values.
-	/// </summary>
-	/// <typeparam name="TKey">The type of the keys in the dictionary. Keys must be non-nullable.</typeparam>
-	/// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
-	/// <param name="items">The dictionary to clone. This dictionary cannot be null.</param>
-	/// <returns>A new dictionary with the same keys and values as the input dictionary.</returns>
-	/// <exception cref="ArgumentNullException">Thrown if the <paramref name="items"/> dictionary is null.</exception>
-	public static IDictionary<TKey, TValue> ShallowClone<TKey, TValue>(this IDictionary<TKey, TValue> items)
-		where TKey : notnull
-	{
-		ArgumentNullException.ThrowIfNull(items);
-		return items.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
-	}
-
-	/// <summary>
-	/// Creates a shallow clone of a dictionary, returning a new dictionary with the same keys and values,
-	/// with thread-safety ensured by locking on the provided object.
-	/// </summary>
-	/// <typeparam name="TKey">The type of the keys in the dictionary. Keys must be non-nullable.</typeparam>
-	/// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
-	/// <param name="items">The dictionary to clone. This dictionary cannot be null.</param>
-	/// <param name="lockObj">The object to lock on to ensure thread-safety during the cloning operation. This cannot be null.</param>
-	/// <returns>A new dictionary with the same keys and values as the input dictionary.</returns>
-	/// <exception cref="ArgumentNullException">
-	/// Thrown if the <paramref name="items"/> dictionary or the <paramref name="lockObj"/> is null.
-	/// </exception>
-	public static IDictionary<TKey, TValue> ShallowClone<TKey, TValue>(this IDictionary<TKey, TValue> items, object lockObj)
-		where TKey : notnull
-	{
-		ArgumentNullException.ThrowIfNull(items);
-		ArgumentNullException.ThrowIfNull(lockObj);
-		lock (lockObj)
-		{
-			return ShallowClone(items);
-		}
 	}
 
 	/// <summary>
--- Extensions/EnumerableExtensions.cs
+++ Extensions/EnumerableExtensions.cs
@@ -6,8 +6,6 @@ namespace ktsu.Extensions;
 
 using System.Collections.ObjectModel;
 
-using ktsu.DeepClone;
-
 /// <summary>
 /// Extension methods for enumerables.
 /// </summary>
@@ -100,88 +98,6 @@ public static class EnumerableExtensions
 		lock (lockObj)
 		{
 			enumerable.ForEach(action);
-		}
-	}
-
-	/// <summary>
-	/// Creates a deep clone of a collection of items, returning a new collection of the specified type.
-	/// </summary>
-	/// <typeparam name="TItem">The type of the items in the collection, which must implement <see cref="IDeepCloneable{TItem}"/>.</typeparam>
-	/// <typeparam name="TDest">The type of the destination collection, which must implement <see cref="ICollection{TItem}"/> and have a parameterless constructor.</typeparam>
-	/// <param name="items">The collection of items to clone. This collection cannot be null.</param>
-	/// <returns>A new collection of type <typeparamref name="TDest"/> containing deep clones of the items in the input collection.</returns>
-	/// <exception cref="ArgumentNullException">Thrown if the <paramref name="items"/> collection is null.</exception>
-	public static TDest DeepClone<TItem, TDest>(this IEnumerable<TItem> items)
-		where TItem : class, IDeepCloneable<TItem>
-		where TDest : ICollection<TItem>, new()
-	{
-		ArgumentNullException.ThrowIfNull(items);
-		var destination = new TDest();
-		destination.AddMany(items.Select(i => i.DeepClone()));
-		return destination;
-	}
-
-	/// <summary>
-	/// Creates a deep clone of a collection of items, returning a new collection of the specified type, 
-	/// with thread-safety ensured by locking on the provided object.
-	/// </summary>
-	/// <typeparam name="TItem">The type of the items in the collection, which must implement <see cref="IDeepCloneable{TItem}"/>.</typeparam>
-	/// <typeparam name="TDest">The type of the destination collection, which must implement <see cref="ICollection{TItem}"/> and have a parameterless constructor.</typeparam>
-	/// <param name="items">The collection of items to clone. This collection cannot be null.</param>
-	/// <param name="lockObj">The object to lock on to ensure thread-safety during the cloning operation. This cannot be null.</param>
-	/// <returns>A new collection of type <typeparamref name="TDest"/> containing deep clones of the items in the input collection.</returns>
-	/// <exception cref="ArgumentNullException">
-	/// Thrown if the <paramref name="items"/> collection or the <paramref name="lockObj"/> is null.
-	/// </exception>
-	public static TDest DeepClone<TItem, TDest>(this IEnumerable<TItem> items, object lockObj)
-		where TItem : class, IDeepCloneable<TItem>
-		where TDest : ICollection<TItem>, new()
-	{
-		ArgumentNullException.ThrowIfNull(items);
-		ArgumentNullException.ThrowIfNull(lockObj);
-		lock (lockObj)
-		{
-			return DeepClone<TItem, TDest>(items);
-		}
-	}
-
-	/// <summary>
-	/// Creates a shallow clone of a collection of items, returning a new collection of the specified type.
-	/// </summary>
-	/// <typeparam name="TItem">The type of the items in the collection.</typeparam>
-	/// <typeparam name="TDest">The type of the destination collection, which must implement <see cref="ICollection{TItem}"/> and have a parameterless constructor.</typeparam>
-	/// <param name="items">The collection of items to clone. This collection cannot be null.</param>
-	/// <returns>A new collection of type <typeparamref name="TDest"/> containing the same items as the input collection.</returns>
-	/// <exception cref="ArgumentNullException">Thrown if the <paramref name="items"/> collection is null.</exception>
-	public static TDest ShallowClone<TItem, TDest>(this IEnumerable<TItem> items)
-		where TDest : ICollection<TItem>, new()
-	{
-		ArgumentNullException.ThrowIfNull(items);
-		var destination = new TDest();
-		destination.AddMany(items);
-		return destination;
-	}
-
-	/// <summary>
-	/// Creates a shallow clone of a collection of items, returning a new collection of the specified type,
-	/// with thread-safety ensured by locking on the provided object.
-	/// </summary>
-	/// <typeparam name="TItem">The type of the items in the collection.</typeparam>
-	/// <typeparam name="TDest">The type of the destination collection, which must implement <see cref="ICollection{TItem}"/> and have a parameterless constructor.</typeparam>
-	/// <param name="items">The collection of items to clone. This collection cannot be null.</param>
-	/// <param name="lockObj">The object to lock on to ensure thread-safety during the cloning operation. This cannot be null.</param>
-	/// <returns>A new collection of type <typeparamref name="TDest"/> containing the same items as the input collection.</returns>
-	/// <exception cref="ArgumentNullException">
-	/// Thrown if the <paramref name="items"/> collection or the <paramref name="lockObj"/> is null.
-	/// </exception>
-	public static TDest ShallowClone<TItem, TDest>(this IEnumerable<TItem> items, object lockObj)
-		where TDest : ICollection<TItem>, new()
-	{
-		ArgumentNullException.ThrowIfNull(items);
-		ArgumentNullException.ThrowIfNull(lockObj);
-		lock (lockObj)
-		{
-			return ShallowClone<TItem, TDest>(items);
 		}
 	}
 
